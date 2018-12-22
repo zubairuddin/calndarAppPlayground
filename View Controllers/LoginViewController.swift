acfec41c74facf1cc630fb3e9af865bf76216060
Lance Owide <lanceowide@gmail.com>
2018-12-22 20:18:59 +0000

    commit

--- View Controllers/LoginViewController.swift
+++ View Controllers/LoginViewController.swift
@@ -11,6 +11,10 @@ import Firebase
 
 class LoginViewController: UIViewController {
 
+
+    
+    
+    
     @IBOutlet var loginEmail: UITextField!
     @IBOutlet var loginPassword: UITextField!
     
@@ -18,46 +22,18 @@ class LoginViewController: UIViewController {
         super.viewDidLoad()
         
 
-    
-
-
+        
     
 }
-////    sign in with phone number
-//        @IBAction func logInPressed(_ sender: UIButton) {
-//
-//            let verificationID = UserDefaults.standard.string(forKey: "authVerificationID")
-//
-//            let credential = PhoneAuthProvider.provider().credential(
-//                withVerificationID: verificationID!,
-//                verificationCode: self.verificationCode.text!)
-//
-//            Auth.auth().signInAndRetrieveData(with: credential) { (authResult, error) in
-//                if error != nil {
-//                    // ...
-//                    return
-//                }
-//
-//                self.performSegue(withIdentifier: "registerComplete", sender: self)
-//
-//                print("signed in")
-//                // User is signed in
-//                // ...
-//            }
-//        }
-    
-//original login with email and passowrd
+
+
     @IBAction func logInPressed(_ sender: UIButton) {
 
-        Auth.auth().signIn(withEmail: loginEmail.text!, password: loginPassword.text!) { (user, error) in
-            if error != nil{
-                print(error!)
-            }else{
-                print("Log In Complete")
+       
+        
                 self.performSegue(withIdentifier: "logInComplete", sender: self)
             }
 
         }
-    }
     
-}
+
